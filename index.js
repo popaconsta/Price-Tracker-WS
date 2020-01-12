0e4cd4fb61cd9852db835c194ef3ad596896f57d
Constantin Popa <popa.constantin00@gmail.com>
2020-01-12 13:55:51 +0100

    add basic mvc structure

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGOLAB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("sample_weatherdata").collection("data");
-  // perform actions on the collection object
-  console.log(collection)
-  client.close();
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = process.env.MONGOLAB_URI;
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("sample_weatherdata").collection("data");
+//   // perform actions on the collection object
+//   console.log(collection)
+//   client.close();
+// });
+//
+//
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const routes = require('./routes'); // Imports routes for the products
+// const app = express();
+// app.use('/', routes);
+// let port = 1234;
+// app.listen(port, () => {
+//     console.log('Server is up and running on port numner ' + port);
+// });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes'); // Imports routes for the products
+const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose')
+const mongoDB = process.env.MONGODB_URI
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', routes);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
 });
