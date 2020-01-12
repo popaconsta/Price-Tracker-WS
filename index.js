4225370c25d5a3410d4afb38e15f386dcbf02d28
Constantin Popa <popa.constantin00@gmail.com>
2020-01-12 14:06:52 +0100

    fix port number bug

--- index.js
+++ index.js
@@ -20,26 +20,26 @@
 // });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const routes = require('./routes'); // Imports routes for the products
+const routes = require('./routes')
 const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose')
 const mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true})
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/', routes);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use('/', routes)
 
-let port = 1234;
+let port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port numner ' + port)
 });
